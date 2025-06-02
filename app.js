f59d6d24d89bc66c01bb4f2b775ca1045a3b7510
mgrlabs <36826483+mcgoldrm@users.noreply.github.com>
2025-06-02 13:01:03 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CompletionDashboard from './CompletionDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <CompletionDashboard />
+    </div>
+  );
+}
+
+export default App;
